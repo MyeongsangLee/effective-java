85a3304884d71cbd56df130f7dc32af8cba4b89a
Lee Myeongsang <mslee0052@gmail.com>
2022-12-28 08:56:00 +0900

    계층형 빌더

--- src/main/java/me/whiteship/chapter01/item02/heirachicalbuilder/Pizza.java
+++ src/main/java/me/whiteship/chapter01/item02/heirachicalbuilder/Pizza.java
@@ -3,6 +3,7 @@ package me.whiteship.chapter01.item02.heirachicalbuilder;
 import java.util.EnumSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Supplier;
 
 // 코드 2-4 계층적으로 설계된 클래스와 잘 어울리는 빌더 패턴(19쪽)
 //
@@ -10,7 +11,7 @@ public class Pizza {
     public enum Topping {HAM, MUSHROOM, ONION, PEPPER, SAUSAGE}
     final Set<Topping> toppings;
 
-    abstract static class Builder<T extends Builder<T>> extends Builder<Builder> {
+    abstract static class Builder<T extends Builder<T>> {
         EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
 
         public T addTopping(Topping topping) {
