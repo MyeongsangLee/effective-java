331f68d49203aa3cbd9afb758b562382e550de90
Lee Myeongsang <mslee0052@gmail.com>
2022-12-27 09:59:26 +0900

    계층형 빌더2

--- src/main/java/me/whiteship/chapter01/item02/heirachicalbuilder/NyPizza.java
+++ src/main/java/me/whiteship/chapter01/item02/heirachicalbuilder/NyPizza.java
@@ -1,2 +1,37 @@
-package me.whiteship.chapter01.item02.heirachicalbuilder;public class NyPizza {
+package me.whiteship.chapter01.item02.heirachicalbuilder;
+
+import java.util.Objects;
+
+public class NyPizza extends Pizza{
+    public enum Size {SMALL, MEDIUM, LARGE};
+    private final Size size;
+
+    public static class Builder extends Pizza.Builder<Pizza.Builder> {
+        private final Size size;
+
+        public Builder(Size size) {
+            this.size = Objects.requireNonNull(size);
+        }
+
+        @Override
+        public Pizza build() {
+            return new NyPizza(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+    }
+
+
+    private NyPizza(Builder builder) {
+        super(builder);
+        size = builder.size;
+    }
+
+    @Override
+    public String toString() {
+        return toppings + "로 토핑한 뉴욕 피자";
+    }
 }
--- src/main/java/me/whiteship/chapter01/item02/heirachicalbuilder/Pizza.java
+++ src/main/java/me/whiteship/chapter01/item02/heirachicalbuilder/Pizza.java
@@ -1,2 +1,30 @@
-package me.whiteship.chapter01.item02.heirachicalbuilder;public class Pizza {
+package me.whiteship.chapter01.item02.heirachicalbuilder;
+
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
+// 코드 2-4 계층적으로 설계된 클래스와 잘 어울리는 빌더 패턴(19쪽)
+//
+public class Pizza {
+    public enum Topping {HAM, MUSHROOM, ONION, PEPPER, SAUSAGE}
+    final Set<Topping> toppings;
+
+    abstract static class Builder<T extends Builder<T>> extends Builder<Builder> {
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+
+        public T addTopping(Topping topping) {
+            toppings.add(Objects.requireNonNull(topping));
+            return self();
+        }
+
+        abstract Pizza build();
+
+        protected abstract T self();
+    }
+
+    Pizza(Builder<?> builder) {
+        toppings = builder.toppings.clone();
+    }
+
 }
